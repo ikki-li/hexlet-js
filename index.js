d769e5a4d5f912c60a107d54cf5b332a94f50f92
Alexandra Yakshina <alexandra.yakshina@gmail.com>
2022-11-30 14:09:09 +0300

    add lodash

---
+++ index.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+
+console.log(_.intersection([1, 2, 5, 10, 23], [2, 4, 6, 5, 11, 22, 23]));
+console.log(_.capitalize('sUb sPECies AEternitatis'));
+console.log(_.last(['one', 'two', 'three']));
+
